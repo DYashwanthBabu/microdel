4151fd107c7d9b029f9c1ef458f5ab159465b459
DYashwanthBabu <90260206+DYashwanthBabu@users.noreply.github.com>
2022-10-13 10:55:34 +0530

    Update task-awinstall.ps1

--- task-awinstall.ps1
+++ task-awinstall.ps1
@@ -1,4 +1,4 @@
 md c:\data
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-wget -OutFile d:\Adventureworks.bak https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2016.bak
+wget -OutFile c:\Adventureworks.bak https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2016.bak
 sqlcmd -E -S localhost -d master -Q "RESTORE DATABASE Adventureworks FROM Disk='d:\AdventureWorks.bak' WITH MOVE 'AdventureWorks2016_Data' TO 'c:\data\adventureworks.mdf', MOVE 'AdventureWorks2016_Log' TO 'c:\data\Adventureworks.ldf' "
